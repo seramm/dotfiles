82c5b6545443d1b5a882b025cada03f337a2657f
seramm <sergioaramburu@tutanota.com>
2022-08-22 16:18:57 +0200

    Added keybinds for navigation

--- config/nvim/lua/user/keys.lua
+++ config/nvim/lua/user/keys.lua
@@ -10,13 +10,23 @@ vim.g.maplocalleader = " "
 
 keymap("n", "<C-Space>", "<cmd>WhichKey<cr>", opts)
 
+
+-- Splits --
+-- Navigation 
+keymap("n", "<m-h>", "<C-w>h", opts)
+keymap("n", "<m-j>", "<C-w>j", opts)
+keymap("n", "<m-k>", "<C-w>k", opts)
+keymap("n", "<m-l>", "<C-w>l", opts)
+
+
+-- Telescope -- 
 -- Find files using Telescope command-line sugar.
 keymap("n", "ff", "<cmd>Telescope find_files<cr>", opts)
 keymap("n", "fg", "<cmd>Telescope live_grep<cr>", opts)
 keymap("n", "fb", "<cmd>Telescope buffers<cr>", opts)
 keymap("n", "fh", "<cmd>Telescope help_tags<cr>", opts)
 
--- Nvim Tree
+-- Nvim Tree --
 keymap("n", "<Space>e", "<cmd>NvimTreeToggle<cr>", opts)
 
 return M
