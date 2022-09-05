dba332e87a4155b9e75167a8a80f898fa3c6ec7c
seram <sergioaramburu@tutanota.com>
2022-09-05 19:53:52 +0200

    Unnecesary lines

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -41,9 +41,6 @@ end)
 beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "kitty"
-editor = os.getenv("EDITOR") or "nvim"
-editor_cmd = terminal .. " -e " .. editor
 
 -- Default modkey.
 -- Usually, Mod4 is the key with a logo between Control and Alt.
