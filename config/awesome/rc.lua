b61e6da9fe982a011239763d96a63174040ab99f
seram <sergioaramburu@tutanota.com>
2022-09-07 20:11:50 +0200

    Fixed another error

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -1,20 +1,11 @@
--- awesome_mode: api-level=4:screen=on
--- If LuaRocks is installed, make sure that packages installed through it are
--- found (e.g. lgi). If LuaRocks is not installed, do nothing.
 pcall(require, "luarocks.loader")
-
 local gears = require("gears")
 local beautiful = require("beautiful")
 
-require("configuration")
-require("interface")
-
-
 beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
 
-
-
-
+require("configuration")
+require("interface")
 
 -- Enable sloppy focus, so that focus follows mouse.
 client.connect_signal("mouse::enter", function(c)
