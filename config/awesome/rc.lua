5812bda6013a84c56a818dc4f9f2c31845d8d95e
seram <sergioaramburu@tutanota.com>
2022-09-15 11:51:30 +0200

    Just testing

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -7,4 +7,15 @@ beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
 require("configuration")
 require("interface")
 
+local function autostart()
+  local helpers = require("helpers")
+  local awful = require("awful")
+  local fylesystem = require("gears.filesystem")
+  local config_dir = fylesystem.get_configuration_dir()
+  helpers.run_once("picom --experimental-backends", function()
+    awful.spawn("picom --experimental-backends --config ".. config_dir .. "configuration/picom.conf", false)
+  end)
 
+end 
+
+autostart()
