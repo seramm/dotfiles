36ea8ea73885b9c22c451524ad841fe7e0aa3dad
seram <sergioaramburu@tutanota.com>
2022-09-18 22:48:14 +0200

    a

--- config/awesome/interface/wibar.lua
+++ config/awesome/interface/wibar.lua
@@ -1,4 +1,6 @@
 local awful = require("awful")
+local beautiful = require("beautiful")
+local gears = require("gears")
 local wibox = require("wibox")
 
 local modkey = "Mod4"
@@ -32,7 +34,22 @@ screen.connect_signal("request::desktop_decoration", function(s)
   s.mytaglist = awful.widget.taglist {
     screen  = s,
     filter  = awful.widget.taglist.filter.all,
-    buttons = taglist_buttons
+    buttons = {
+      awful.button({ }, 1, function(t) t:view_only() end),
+      awful.button({ modkey }, 1, function(t)
+                                      if client.focus then
+                                          client.focus:move_to_tag(t)
+                                      end
+                                  end),
+      awful.button({ }, 3, awful.tag.viewtoggle),
+      awful.button({ modkey }, 3, function(t)
+                                      if client.focus then
+                                          client.focus:toggle_tag(t)
+                                      end
+                                  end),
+      awful.button({ }, 4, function(t) awful.tag.viewprev(t.screen) end),
+      awful.button({ }, 5, function(t) awful.tag.viewnext(t.screen) end),
+    }
   }
 
   -- Create a tasklist widget
@@ -53,6 +70,7 @@ screen.connect_signal("request::desktop_decoration", function(s)
   s.mywibox = awful.wibar {
     position = "top",
     screen   = s,
+    bg = beautiful.bg_normal .. "55",
     widget   = {
       layout = wibox.layout.align.horizontal,
       { -- Left widgets
