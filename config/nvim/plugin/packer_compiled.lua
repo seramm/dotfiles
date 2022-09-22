898569a7377897340f8444accd2e32d522c310c2
seram <sergioaramburu@tutanota.com>
2022-09-22 19:36:36 +0200

    Packer

--- config/nvim/plugin/packer_compiled.lua
+++ config/nvim/plugin/packer_compiled.lua
@@ -145,6 +145,11 @@ _G.packer_plugins = {
     path = "/home/seram/.local/share/nvim/site/pack/packer/start/mason.nvim",
     url = "https://github.com/williamboman/mason.nvim"
   },
+  ["null-ls.nvim"] = {
+    loaded = true,
+    path = "/home/seram/.local/share/nvim/site/pack/packer/start/null-ls.nvim",
+    url = "https://github.com/jose-elias-alvarez/null-ls.nvim"
+  },
   ["nvim-autopairs"] = {
     loaded = true,
     path = "/home/seram/.local/share/nvim/site/pack/packer/start/nvim-autopairs",
