b22322fe4f6e806bf2e3609f1f534f5367fe1280
seram <sergioaramburu@tutanota.com>
2022-09-23 12:17:55 +0200

    Added theme file

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -2,7 +2,7 @@ pcall(require, "luarocks.loader")
 local beautiful = require("beautiful")
 local gears = require("gears")
 
-beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
+beautiful.init(gears.filesystem.get_configuration_dir() .. "interface/theme/theme.lua")
 
 require("configuration")
 require("interface")
