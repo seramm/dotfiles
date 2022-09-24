e621a65df9790d12a5f0b4840c21c5d9e31fc0fb
seramm <sergioaramburu@tutanota.com>
2022-09-24 11:25:36 +0200

    Added autofocus

--- config/awesome/rc.lua
+++ config/awesome/rc.lua
@@ -1,6 +1,7 @@
 pcall(require, "luarocks.loader")
 local beautiful = require("beautiful")
 local gears = require("gears")
+require("awful.autofocus")
 
 beautiful.init(gears.filesystem.get_configuration_dir() .. "interface/theme/theme.lua")
 
