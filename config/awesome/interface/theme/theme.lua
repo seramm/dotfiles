7b4d5dcd3b41f9fd05fd57776454faddb875e262
seramm <sergioaramburu@tutanota.com>
2022-10-25 14:07:38 +0200

    Changed systray background

--- config/awesome/interface/theme/theme.lua
+++ config/awesome/interface/theme/theme.lua
@@ -20,7 +20,7 @@ theme.bg_normal     = "#222222"
 theme.bg_focus      = "#535d6c"
 theme.bg_urgent     = "#ff0000"
 theme.bg_minimize   = "#444444"
-theme.bg_systray    = theme.bg_normal
+theme.bg_systray    = "#3A475C"
 
 theme.fg_normal     = "#aaaaaa"
 theme.fg_focus      = "#ffffff"
