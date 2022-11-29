6bc72dde72dd75babd062ea0ce3676f210049de4
seramm <sergioaramburu@tutanota.com>
2022-11-29 12:23:28 +0100

    a

--- config/nvim/lua/user/bufferline.lua
+++ config/nvim/lua/user/bufferline.lua
@@ -1,8 +1,8 @@
 local status_ok, bufferline = pcall(require, "bufferline")
-if not status_ok then 
+if not status_ok then
   return
 end
-  
+
 bufferline.setup {
   options = {
     numbers = "none",
@@ -11,8 +11,8 @@ bufferline.setup {
     left_mouse_command = "buffer %d",
     middle_mouse_command = "nil",
     indicator = { icon = "│", style = "icon" },
-    buffer_close_icon = '', 
-    modified_icon = "●", 
+    buffer_close_icon = '',
+    modified_icon = "●",
     close_icon = "",
     left_trunc_marker = "",
     right_trunc_marker = "",
