33fb6b00dce93a4af9ffdc88fbbb7d2b2b59ee76
Sergio Aramburu <sergioaramburu@tutanota.com>
2023-03-12 15:37:56 +0100

    Remake of plugins.lua with basic packages

--- config/nvim/lua/user/plugins.lua
+++ config/nvim/lua/user/plugins.lua
@@ -1,36 +1,30 @@
-local fn = vim.fn
-
--- Automatically install packer
-local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
----@diagnostic disable-next-line: missing-parameter
-if fn.empty(fn.glob(install_path)) > 0 then
-  PACKER_BOOTSTRAP = fn.system {
-    "git",
-    "clone",
-    "--depth",
-    "1",
-    "https://github.com/wbthomason/packer.nvim",
-    install_path,
-  }
-  print "Installing packer close and reopen Neovim..."
-  vim.cmd [[packadd packer.nvim]]
-end
-
--- Autocommand that reloads neovim whenever you save the plugins.lua file
-vim.cmd [[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins.lua source <afile> | PackerSync
-  augroup end
-]]
-
--- Use a protected call so we don't error out on first use
+-- Packer & Plugins Config
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
   return
 end
 
--- Have packer use a popup window
+local fn = vim.fn
+
+local ensure_packer = function()
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+vim.cmd([[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]])
+
 packer.init {
   -- snapshot = "july-24",
   snapshot_path = fn.stdpath "config" .. "/snapshots",
@@ -43,17 +37,20 @@ packer.init {
   },
 }
 
--- Install your plugins here
-return packer.startup(function(use)
-  -- Plugin Mangager
-  use "wbthomason/packer.nvim" -- Have packer manage itself
 
-  -- PLUGINS GO HERE
+return require('packer').startup(function(use)
+  -- Packer Plugin
+  use 'wbthomason/packer.nvim'
 
+  -- Basic Plugins
+  use 'nvim-tree/nvim-web-devicons'
+  
+  --Appereance
+  use 'folke/tokyonight.nvim'
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
-  if PACKER_BOOTSTRAP then
-    require("packer").sync()
+  if packer_bootstrap then
+    require('packer').sync()
   end
 end)
